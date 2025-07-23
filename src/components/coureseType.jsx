447cd28215bb5268a5dfd4d704b5db20b20032af
Kar9ti8k <karthiksankineni07@gmail.com>
2025-07-23 13:31:53 +0530

    install the tailwindcss

--- src/components/coureseType.jsx
+++ src/components/coureseType.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const CoureseType = () => {
-  return <div>coureseType</div>
+  return (
+    <>
+      <div className='main-box'>
+        <h1>Course Type</h1>
+      </div>
+    </>
+  )
 }
 
 export default CoureseType
