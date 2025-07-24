995b8703b047055be5c58b6c0a886aaa1bc81aab
Kar9ti8k <karthiksankineni07@gmail.com>
2025-07-24 15:23:43 +0530

    comeleted the coureses page

--- src/components/courseManager.jsx
+++ src/components/courseManager.jsx
@@ -8,8 +8,8 @@ const CourseManager = () => {
   const [editIndex, setEditIndex] = useState(null)
   const [editValue, setEditValue] = useState('')
   const addCourse = () => {
-    if (newCourse && !courses) {
-      setCourses([...CourseManager, newCourse])
+    if (newCourse && !courses.includes(newCourse)) {
+      setCourses([...courses, newCourse])
       setNewCourse('')
     }
   }
@@ -29,7 +29,12 @@ const CourseManager = () => {
       <div className='h-[14rem] bg-white w-[25rem] flex flex-col items-start rounded-sm shadow-2xs'>
         <h1 className='m-2 pl-2'>Courses</h1>
         <div className='pl-4 flex  '>
-          <input type='text' className='bg-blue-50 w-[18rem] h-8 ' />
+          <input
+            type='text'
+            className='bg-blue-50 w-[18rem] h-8 '
+            value={newCourse}
+            onChange={(e) => setNewCourse(e.target.value)}
+          />
           <button
             className='bg-blue-500 text-white ml-2 w-[4rem] rounded-sm'
             onClick={addCourse}
@@ -70,7 +75,7 @@ const CourseManager = () => {
                     onClick={() => deleteCourse(index)}
                     className='text-red-600'
                   >
-                    Delete
+                    &times;
                   </button>
                 </>
               )}
